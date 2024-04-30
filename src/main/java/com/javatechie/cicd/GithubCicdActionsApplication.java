a82d2ba67cacf5f38faaeb9eb39554556c343b3b
dhlabanu <hlabanuden@gmail.com>
2024-04-30 22:35:24 +0200

    code

---
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,29 @@
+package com.javatechie.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to javatechie !";
+    }
+
+//    echo "# github-actions-example" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/hlabanud1/github-actions-example.git
+//    git push -u origin main
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
+
+}
